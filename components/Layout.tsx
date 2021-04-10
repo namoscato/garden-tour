52cabf187ecba9ef11ad18d868b6ca51bcf5e391
Nick Amoscato <nick@amoscato.com>
2021-04-10 17:44:37 -0400

    Configure Google Analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -53,7 +53,7 @@ export default function Layout({ children }: Props) {
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
       </Head>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
+import * as gtag from "lib/gtag";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import "node_modules/normalize.css/normalize.css";
+import { useEffect } from "react";
 import "styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageView(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
