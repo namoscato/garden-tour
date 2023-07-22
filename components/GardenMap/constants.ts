cf366aebd0b65c60c91c19a1f689f35dc96993f8
Nick Amoscato <nick@amoscato.com>
2023-07-21 23:19:59 -0400

    Disable clickable points of interest

--- components/GardenMap/constants.ts
+++ components/GardenMap/constants.ts
@@ -3,6 +3,7 @@ import { MapOptions } from "google-map-react";
 export const MAP_OPTIONS: MapOptions = {
   fullscreenControl: false,
   mapTypeId: "terrain",
+  clickableIcons: false,
 };
 
 export const BREAKPOINT_DESKTOP = 800;
