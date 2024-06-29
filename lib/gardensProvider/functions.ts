060793dbf29c6ebbcfe6eea386073ef3ac2d1120
Nick Amoscato <nick@amoscato.com>
2024-06-29 15:31:44 -0400

    Populate 2024 guide (#268)

--- lib/gardensProvider/functions.ts
+++ lib/gardensProvider/functions.ts
@@ -70,7 +70,9 @@ export function gardenFromRow(row: GoogleSpreadsheetRow): Garden {
     number: Number(row.get(SheetColumn.Number)),
     address: row.get(SheetColumn.Address),
     description: row.get(SheetColumn.Description),
-    participation: PARTICIPATION_OPTIONS.filter((option) => row.get(option)),
+    participation: PARTICIPATION_OPTIONS.filter(
+      (option) => "FALSE" !== row.get(option)
+    ),
     location: {
       lat: Number(row.get(SheetColumn.Lat)),
       lng: Number(row.get(SheetColumn.Lng)),
