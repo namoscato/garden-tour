0dedc4218c9def256dab0eb49818101c5815c96a
Nick Amoscato <nick@amoscato.com>
2026-05-10 11:22:14 -0400

    Expose 2026 registration (#385)

--- components/Dates/constants.ts
+++ components/Dates/constants.ts
@@ -1,7 +1,7 @@
 import { ParticipationOption, SheetColumn } from "lib/gardensProvider/types";
 import { ParticipationDate } from "./types";
 
-export const YEAR = 2025;
+export const YEAR = 2026;
 
 export const MONTH = "July";
 
@@ -9,24 +9,23 @@ export const TIME_NIGHT = "8:30pm – 11pm";
 
 export const TIME_DAY = "10am – 4pm";
 
-export const PARTICIPATION_DATE: Record<
-  ParticipationOption,
-  ParticipationDate
+export const PARTICIPATION_DATE: Readonly<
+  Record<ParticipationOption, ParticipationDate>
 > = {
   [SheetColumn.ParticipationFridayNight]: {
-    day: 18,
+    day: 17,
     time: TIME_NIGHT,
   },
   [SheetColumn.ParticipationSaturdayDay]: {
-    day: 19,
+    day: 18,
     time: TIME_DAY,
   },
   [SheetColumn.ParticipationSaturdayNight]: {
-    day: 19,
+    day: 18,
     time: TIME_NIGHT,
   },
   [SheetColumn.ParticipationSundayDay]: {
-    day: 20,
+    day: 19,
     time: TIME_DAY,
   },
 };
